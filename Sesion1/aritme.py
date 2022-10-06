4c29f2de9abec22b1a94b21a18402551008ec4b9
Silvinho Morales <76801207+Silvinhom2@users.noreply.github.com>
2022-10-05 23:16:54 -0500

    Add files via upload

---
+++ Sesion1/aritme.py
@@ -0,0 +1,24 @@
+#programa para uso de operadores aritméticos 
+
+edad =25
+print('el tipo de edad es: ', type(edad))
+precio = 88.40
+print('el tipo de precio es: ', type(precio))
+nombre = 'Alicia'
+print('el tipo de nombre es: ', type(nombre))
+es_blanco = True
+print('el tipo de es_blanco es: ', type(es_blanco))
+
+a = 10
+b = 3
+
+print('La suma de a+b es: '+ str(a+b))
+print('La resta de a-b es: '+ str(a-b))
+print('La multiplicación de a*b es: '+ str(a*b))
+print('La división de a/b es: '+ str(a/b))
+print(f'La división de a/b es: + (a/b:.2f)')
+print(f'La división de a/b es: ', round(a/b,2))
+print('El modulo de a%b es: '+ str(a%b))
+print('El modulo de a%b es: '+ str(a%b))
+print('La división entera de a//b es: '+ str(a//b))
+print('La potencia de a**b es: '+ str(a**b))
---
+++ Sesion1/aritme2.py
@@ -0,0 +1,12 @@
+# precedencia de operadores
+
+# {}
+# **
+# * / % //
+# + -
+# jararquia es igual, asociatividad de izquierda a derecha
+
+#Los paréntesis hace una gran diferencia a la hora de ecuaciones grandes
+print('aplicando potencia:', (4+5)*2**3)
+print('aplicando potencia:', 4+5*2**3)
+print('aplicando potencia:', 10/5*2)
