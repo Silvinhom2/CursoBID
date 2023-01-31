86916f270242af83f1e3be8acd4422729ad3a32d
Silvinho Morales <76801207+Silvinhom2@users.noreply.github.com>
2023-01-30 21:00:56 -0600

    last update

--- Sesion4/tareas/ejercicio3.py
+++ Sesion4/tareas/ejercicio3.py
@@ -1,8 +1,8 @@
 #'''Escribir la función titulo(), la cual recibe un string y lo retorna convirtiendo la primera letra de cada palabra a mayúscula y las demás letras a minúscula, dejando inalterados los demás caracteres. Precondición: el separador de palabras es el espacio: " ". No usar la función title de Python'''
 
 
+oracion = input('Ingrese alguna oración!: ')
 def titulo(oracion):
-    oracion = ""
     first_word = True
     for letra in oracion:
         if not letra.isalpha():
@@ -15,4 +15,5 @@ def titulo(oracion):
             else:
                 oracion = oracion + letra.lower()
     return oracion
-        
+
+print(titulo(oracion))
